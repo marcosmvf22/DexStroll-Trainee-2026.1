0e5cdbd98292a1049cc5485206574729c2543bde
izzyadora <souzad.isadora@gmail.com>
2026-05-21 20:18:24 -0300

    chore: adicionadas funções de abrir e fechar modal

---
+++ public/script.js
@@ -0,0 +1,7 @@
+function abrirModal(idModal){
+    document.getElementById(idModal).style.display = "flex";
+}
+
+function fecharModal(idModal){
+    document.getElementById(idModal).style.display = "none";
+}
