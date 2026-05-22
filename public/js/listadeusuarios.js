48fbeb146601e9318a5eb2b141bc34cfd3372f2f
paqgamer <p.a.qgamer@gmail.com>
2026-05-22 17:30:44 -0300

    feat: torna listagem dinamica de usuarios na tabela
    -copia código da admin dashboard (temporariamente)
    -proxima alteração será para implementar o modal

--- public/js/listadeusuarios.js
+++ public/js/listadeusuarios.js
@@ -1,5 +1,3 @@
-//Infelizmente o JS está tirando minha sanidade, mas acho que consigo
-//parece que a sintaxe é maneira até certo ponto
 const listaUsuarios = [
   {
     username: "Usuarioexemplo1",
@@ -18,31 +16,24 @@ const listaUsuarios = [
   },
 ];
 
-//aparentemente declarar variável dinamica é bem melhor que no java, no java eu tinha que declarar strings etc de forma estática
-
 const corpoTabela = document.getElementById("corpo-tabela-usuarios"); 
 
 if (corpoTabela) {
-  corpoTabela.innerHTML = ""; // Tirar o HTML (mesmo nao sendo  util agora)
+  corpoTabela.innerHTML = ""; 
 
   listaUsuarios.forEach((usuario) => {
-    //eu  vi que esse comentário abaixo pode ajudar minha extensão (prettier) a formatar minhas tags HTML dentro do JS
-    //pelo que entendi a crase faz o html ser injetado deforma linear, resumindo, eu nao entendi, mas funciona
     const linhaHTML = /* html */ `  
-    <tr>
+      <tr>
         <td class="dado_id_admin">${usuario.username}</td>
         <td>${usuario.nome}</td>
         <td>${usuario.email}</td>
-        
         <td class="celula-acoes-admin">
           <button class="botao-acao" title="Visualizar">
             <span class="material-icons">visibility</span>
           </button>
-          
           <button class="botao-acao" title="Editar">
             <span class="material-icons">edit</span>
           </button>
-          
           <button class="botao-acao botao-deletar" title="Deletar">
             <span class="material-icons">delete</span>
           </button>
@@ -50,6 +41,6 @@ if (corpoTabela) {
       </tr>
     `;
 
-    corpoTabela.innerHTML += linhaHTML; //aparentemente a propriedade  cumulativa funciona igual no C++
+    corpoTabela.innerHTML += linhaHTML; 
   });
 }
