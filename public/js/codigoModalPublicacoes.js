0d96a3b57088e350553f78bd17c359c1d3b086be
izzyadora <souzad.isadora@gmail.com>
2026-05-27 16:42:15 -0300

    chore: html e js adaptados para a adição de labels no modal

--- public/js/codigoModalPublicacoes.js
+++ public/js/codigoModalPublicacoes.js
@@ -1,4 +1,7 @@
 const filtro = document.querySelector('#filtroFundoModal');
+const checkbox = document.getElementById('toggle-curiosidade');
+const inputContainer = document.getElementById('input-container');
+
 
 function abrirModal(idModal){
     const modal = document.getElementById(idModal);
@@ -12,3 +15,10 @@ function fecharModal(idModal){
     filtro.style.display = "none";
 }
 
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    inputContainer.classList.remove('hidden'); // Mostra o input
+  } else {
+    inputContainer.classList.add('hidden');    // Esconde o input
+  }
+});
