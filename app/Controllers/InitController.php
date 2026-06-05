f0624543817166b8af91a46136695e0d51e6da3a
izzyadora <souzad.isadora@gmail.com>
2026-06-05 13:38:45 -0300

    chore: 'InitController' criado

---
+++ app/Controllers/InitController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class InitController
+{
+
+    public function index()
+    {
+        return view('site/landingpage');
+        
+    }
+}
