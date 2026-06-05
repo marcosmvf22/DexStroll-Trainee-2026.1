91d2f137ed38ae6017b4d6f1b6390d4936a91e6f
izzyadora <souzad.isadora@gmail.com>
2026-06-05 13:36:26 -0300

    chore: 'routes.php' alterado

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Controllers;
-use App\Controllers\ExampleController;
+use App\Controllers\InitController; //atualizei pra deixar igual a do Pedro
 use App\Core\Router;
 
+
+$router->get('', 'InitController@index');
 $router->get('publicacoes', 'PublicacoesController@index');
 $router->post('publicacoes/edit', 'PublicacoesController@edit');
 $router->post('publicacoes/criar', 'PublicacoesController@store');
-// $router->delete('publicacoes/delete', 'PublicacoesController@delete');
+$router->post('publicacoes/delete', 'PublicacoesController@delete');
