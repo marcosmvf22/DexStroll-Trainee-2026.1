88ddb1f487d0de8ae65f6cfa260da95be5f66a21
paqgamer <p.a.qgamer@gmail.com>
2026-06-06 16:03:49 -0300

    chore: importa controller, css e js da branch PublicacaoCrud

---
+++ app/Controllers/PublicacoesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class PublicacoesController
+{
+
+    public function index()
+    {
+        $publicacoes = App::get('database')->selectAll('publicacao');
+        return view('admin/pagina_publicacoes', compact('publicacoes'));
+    }
+    public function edit()
+    {
+        $parameters = [
+            'titulo' => $_POST['titulo'],
+            'autor' => $_POST['autor'],
+            'data' => $_POST['data'],
+            'conteudo' => $_POST['conteudo'],
+            'curiosidade' => $_POST['curiosidade'],
+            'data' => $_POST['data'],
+        ];
+
+        $id = $_POST['id'];
+        
+        App::get('database')->update('publicacao',$id, $parameters);
+        header('Location: /publicacoes');
+    }
+    public function store()
+    {
+        $parameters = [
+            'titulo' => $_POST['titulo'],
+            'autor' => $_POST['autor'] ?? 1,
+            'data' => $_POST['data'],
+            'conteudo' => $_POST['conteudo'],
+            'curiosidade' => $_POST['curiosidade'],
+            'imagem' => $_POST['imagem'] ?? ''
+        ];
+
+        App::get('database')->insert('publicacao', $parameters);
+        header('Location: /publicacoes');
+    }
+    public function delete()
+    {
+        $id = $_POST['id'];
+
+        App::get('database')->delete('publicacao', $id);
+
+        header('Location: /publicacoes');
+    }
+}
