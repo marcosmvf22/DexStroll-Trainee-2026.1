a77636fea1673cc0ea58e9fbf3d5f5e396566c23
Guilherme Sander <guilhermesander.morais@estudante.ufjf.br>
2026-06-07 02:48:46 -0300

    chore: ajusta js para deixar a posicao do botao toggle no mobile melhor

--- public/js/Sidebar.js
+++ public/js/Sidebar.js
@@ -1,16 +1,25 @@
 const  buttonToggle = document.querySelector(".toggle-sidebar");
+const habilitarSide = document.querySelector(".habilitar_sidebar");
 const sidebar = document.querySelector(".sidebar");
 
-if(buttonToggle && sidebar)
+if(buttonToggle)
 {
     buttonToggle.addEventListener('click',(e)=> {
         e.stopPropagation();
+
         sidebar.classList.toggle('mobile-open');
+
+        if (sidebar.classList.contains('mobile-open')) {
+            habilitarSide.classList.add('sidebar-aberta');
+        } else {
+            habilitarSide.classList.remove('sidebar-aberta');
+        }
     })
 
     document.addEventListener('click',(e)=>{
         if (!sidebar.contains(e.target) && sidebar.classList.contains('mobile-open')) {
             sidebar.classList.remove('mobile-open');
+            habilitarSide.classList.remove('sidebar-aberta');
         }
     })
 }
