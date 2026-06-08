1c8f07a02b1476ae235ba0a5ce0431fd416c0e18
Ana <anaclaralelesquintao@gmail.com>
2026-06-07 23:38:53 -0300

    refactor: integra login e animação

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -5,7 +5,6 @@
     require 'core/bootstrap.php';
 
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -17,25 +16,28 @@
     <!-- Fonte -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <!-- icones -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <main class="main-login">
-        <div class="container-login">
+        <div class="container-principal-login loginActive">
+            <div class="container-secundario-login">
+                <div class="box1-login">
+                    <div class="topo-login">
+                        <i id="icone-voltar-login" class="fa-regular fa-circle-left"></i>
+                        <h1 class="titulo-login">LOGIN</h1>
+                    </div>
 
-            <div class="box1-login">
-                <div class="topo-login">
-                    <i id="icone-voltar-login" class="fa-regular fa-circle-left"></i>
-                    <h1 class="titulo-login">LOGIN</h1>
-                </div>
+                    <h2 class="subtitulo-login">Bem-vindo de volta!</h2>
 
-                <h2 class="subtitulo-login">Bem-vindo de volta!</h2>
-
-                <form action="/login" method="POST">
+                    <form action="/login" method="POST">
                     <div class="mensagem-erro">
                         <?php
                         if (isset($_SESSION['mensagem-erro'])) {
@@ -46,6 +48,7 @@
                         ?>
 
                     </div>
+
                     <div class="campos-login">
                         <p class="descricao-login">Email</p>
                         <label class="label-login">
@@ -66,17 +69,76 @@
                         </label>
                     </div>
 
-                    <button class="botao-login" type="submit">Login</button>
-                </form>
+                    <button class="botao-login">Login</button>
+                    </form>
+                    <p class="texto-login">Não possui uma conta? <a class="texto-link-login">CADASTRE-SE</a></p>
+                </div>
 
-                <p class="texto-login">Não possui uma conta? <a class="texto-link-login" href="">CADASTRE-SE</a></p>
+
+                <div class="box2-login">
+                    <img class="img-login" src="/public/assets/logo.png" alt="">
+                </div>
             </div>
 
-            <div class="box2-login">
-                <img class="img-login" src="/public/assets/logo.png" alt="">
+            <div class="container-secundario-cadastro">
+
+                <div class="box1-cadastro">
+                    <div class="topo-login">
+                        <i id="icone-voltar-cadastro" class="fa-regular fa-circle-left"></i>
+                        <h1 class="titulo-login">Cadastro</h1>
+                    </div>
+
+                    <h2 class="subtitulo-login">Registre-se aqui.</h2>
+
+                    <div class="campos-login">
+                        <p class="descricao-cadastro">Nome</p>
+                        <label class="label-cadastro">
+                            <div class="elementos-input">
+                                <i id="icone-user-cadastro" class="fa-regular fa-user"></i>
+                                <span class="line-cadastro"></span>
+                            </div>
+                            <input id="nome" name="nome" type="nome" class="input-login">
+                        </label>
+
+                        <p class="descricao-cadastro">Email</p>
+                        <label class="label-cadastro">
+                            <div class="elementos-input">
+                                <i id="icone-envelope-cadastro" class="fa-regular fa-envelope"></i>
+                                <span class="line-cadastro"></span>
+                            </div>
+                            <input id="email" name="email" type="email" class="input-login">
+                        </label>
+
+                        <p class="descricao-cadastro">Senha</p>
+                        <label class="label-cadastro">
+                            <div class="elementos-input">
+                                <i id="icone-senha-cadastro" class="fa-solid fa-lock"></i>
+                                <span class="line-cadastro"></span>
+                            </div>
+                            <input id="senha" name="senha" type="senha" class="input-login">
+                        </label>
+
+                        <p class="descricao-cadastro">Confirme a senha</p>
+                        <label class="label-cadastro">
+                            <div class="elementos-input">
+                                <i id="icone-senha-cadastro" class="fa-solid fa-lock"></i>
+                                <span class="line-cadastro"></span>
+                            </div>
+                            <input id="confirmaSenha" name="confirmaSenha" type="confirmaSenha" class="input-login">
+                        </label>
+                    </div>
+
+                    <button class="botao-cadastro">Cadastrar</button>
+                </div>
+
+                <div class="box2-cadastro">
+                    <img class="img-login" src="/public/assets/logo.png" alt="">
+                </div>
             </div>
         </div>
+
     </main>
 </body>
+<script src="/public/js/login.js"></script>
 
 </html>
