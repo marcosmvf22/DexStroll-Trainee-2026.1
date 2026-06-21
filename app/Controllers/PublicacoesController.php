82954d2d16c704b16bb992edab028274f09e6139
paqgamer <p.a.qgamer@gmail.com>
2026-06-20 21:39:17 -0300

    test: renomeia imagens da funcao popularposts, agora é imagem1.jpg, imagem2 etc.... por ai vai

--- app/Controllers/PublicacoesController.php
+++ app/Controllers/PublicacoesController.php
@@ -181,12 +181,13 @@ class PublicacoesController
         // $categorias = $this->getCategorias();
         
         // aqui coloquei pra nao criar com uma mesma imagen
+        // renomeei pra ficar mais faicl de todo mundo se organizar nisso, pequeno detalhe extremamente irrelevvavnte, mas enfim
         $imagens = [
-            'public/assets/imagensPosts/1e3171a2b1082af948342a8ffa2c3b25c70fc16f.jpg',
-            'public/assets/imagensPosts/139bcefeeb7162c5c5218a99e36c3d8b41db9858.jpg',
-            'public/assets/imagensPosts/a4d6fa3b5171af892fd8a6926104947816d3f4a4.jpg',
-            'public/assets/imagensPosts/716564.png',
-            'public/assets/imagensPosts/453024.jpg'
+            'public/assets/imagensPosts/imagem1.jpg',
+            'public/assets/imagensPosts/imagem2.jpg',
+            'public/assets/imagensPosts/imagem3.jpg',
+            'public/assets/imagensPosts/imagem4.jpg',
+            'public/assets/imagensPosts/imagem5.jpg'
         ];
         $titulos = ['Quintino é um cara legal','O retorno da franquia', 'Nova atualização lançada', 'Guia para iniciantes', 'Melhores momentos do ano', 'Análise completa', 'O que esperar do futuro', 'Entrevista exclusiva', 'Rumores confirmados', 'Promoção imperdível', 'Dicas avançadas', 'DexStroll é o melhor blog'];
         
